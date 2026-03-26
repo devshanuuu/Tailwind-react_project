d5f18ae078ff3b8a29b77024dcdc6e9da23b139c
devshanuuu <shanuuu0207@gmail.com>
2026-03-26 18:11:28 +0530

    first commit

---
+++ src/components/section1/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-6 px-16">
+
+        <h4 className="bg-black text-white py-3 px-5 rounded-full">Target Audience</h4>
+        <button className="bg-gray-200 px-6 py-2 uppercase rounded-full tracking-wider text-sm">Digital banking platform</button>
+        </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/section2/Section2.jsx
@@ -0,0 +1,9 @@
+
+
+const section2 = () => {
+  return (
+    <div className='h-screen w-full bg-amber-500'>section2</div>
+  )
+}
+
+export default section2
